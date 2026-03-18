c5db5d6bc11cc54f9266cbd98323fa47b1ea0939
zazajo <Josephedward201@gmail.com>
2026-03-18 09:01:49 +0100

    try

--- crwn/settings.py
+++ crwn/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'crownie.middleware.DiscordConnectionRequiredMiddleware',
+    #'crownie.middleware.DiscordConnectionRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'crwn.urls'
